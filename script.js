68fa63a571078b802aac97c988e6416cb388633a
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-20 15:25:41 +0100

    add script

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('ciao js');
+
+const etaUtente = prompt('quanti anni hai?')
+
+console.log('etaUtente');
+
+let kmPercorsi = prompt('quanti km dovrai percorrere?')
+
+console.log('kmPercorsi');
